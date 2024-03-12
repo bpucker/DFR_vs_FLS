250f7e59b225f185d2bd67a5194b99bb3a6a0c70
Nancy Choudhary <nancychoudhary139@gmail.com>
2024-03-12 12:29:55 +0100

    Update densityhm_marghist.py

--- densityhm_marghist.py
+++ densityhm_marghist.py
@@ -1,44 +1,35 @@
+#Python code to create 2D density heatmap with marginal histograms using any gene IDs of interest and expression data (from count table) 
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
 import matplotlib.colors as mcolors
 
-# Replace 'your_count_table.csv' with the actual file path of your count table
-count_table_path = 'F_N_merged_count_table.txt'
-
-# Read the count table
+count_table_path = '20211016_Vitis_vinifera_phytozome_tpms_clean.txt'
 count_table = pd.read_csv(count_table_path, sep='\t', index_col=0)
 
-gene_x = 'FLS_F'
-# Replace 'GeneX' with the actual gene name you want to plot on the x-axis
-gene_x_values = count_table.loc[gene_x]
+gene_x = 'VIT_218s0001g03490.1' #replace with the actual gene ID you want to plot on the x-axis
+gene_x_values = count_table.loc[genex]
 
-gene_y = 'DFR_N'
-# Replace 'GeneY' with the actual gene name you want to plot on the y-axis
-gene_y_values = count_table.loc[gene_y]
+gene_y = 'VIT_218s0001g12800.1' #Replace with the actual gene ID you want to plot on the y-axis
+gene_y_values = count_table.loc[geney]
 
-def makesweetgraph(x=None, y=None, cmap='gnuplot2_r', ylab=None, xlab=None, bins=25, figsize=(5, 4), snsbins=50):
+def marghist(x=None, y=None, cmap='gnuplot2_r', ylab=None, xlab=None, bins=25, figsize=(5, 4), snsbins=50):
     ax1 = sns.jointplot(x=gene_x_values, y=gene_y_values, color='#cb181d', marginal_kws=dict(bins=snsbins))
     ax1.fig.set_size_inches(figsize[0], figsize[1])
     ax1.ax_joint.cla()
     plt.sca(ax1.ax_joint)
-    # Create a 2D histogram
     plt.hist2d(gene_x_values, gene_y_values, norm=mcolors.LogNorm(), cmap=cmap, bins=bins)
-    # Add sample size (N) as text
     sample_size_text = f'n = {len(gene_x_values)}'
     plt.text(0.5, 0.95, sample_size_text, transform=plt.gca().transAxes, ha='center', va='center', fontsize=10,
              bbox=dict(facecolor='white', alpha=0.8))
-    plt.xlabel('${FLS_\mathrm{F}}$ expression (in TPMs)', fontsize=12)
-    plt.ylabel('${DFR_\mathrm{N}}$ Expression (in TPMs)', fontsize=12)
-    # Add a colorbar
-    cbar_ax = ax1.fig.add_axes([0.2, 0.02, 0.6, 0.02])
-    cb = plt.colorbar(cax=cbar_ax, orientation='horizontal'
-    cb.set_label(r'$\log_{10}$ density of points', fontsize=13)"""
-    # Add label (a) at top left
+    plt.xlabel(r'$\it{' + gene_x + '}$ Expression (in TPMs)', fontsize=12)  
+    plt.ylabel(r'$\it{' + gene_y + '}$ Expression (in TPMs)', fontsize=12)
+    cbar_ax = ax1.fig.add_axes([1, 0.1, .03, .7])
+    cb = plt.colorbar(cax=cbar_ax)
+    cb.set_label(r'$\log_{10}$ density of points', fontsize=13)
+    #plt.suptitle(r'$\it{' + gene_x + '}$ vs $\it{' + gene_y + '}$ Expression')
 
-# Call the function without sets
-makesweetgraph()
+marghist()
 
-plt.savefig("FFvsDN", bbox_inches='tight', dpi=1000)
 plt.show()
